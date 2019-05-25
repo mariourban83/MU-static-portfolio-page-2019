b02c8d5f5ca31b93c5453cfdff4810ebce8f7c9c
mariourban83 <mario.urban.83@gmail.com>
2019-05-25 12:42:00 +0100

    Updated root directory structure

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// Scrolling Effect Dark Background
+
+$(window).on("scroll", function() {
+    if($(window).scrollTop()) {
+          $('nav').addClass('black');
+    }
+
+    else {
+          $('nav').removeClass('black');
+    }
+})
